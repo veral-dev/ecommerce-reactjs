d717dde66ad9306f56574db9a4296b42c8617242
davidvrl <veral.david@gmail.com>
2020-03-05 12:24:24 +0100

    heroku

--- client/src/services/user.services.js
+++ client/src/services/user.services.js
@@ -4,7 +4,7 @@ export default class Services {
 
     constructor() {
         this.service = axios.create({
-            baseURL: 'http://localhost:5000/api/users',
+            baseURL: `${process.env.REACT_APP_URL}/users`,
             withCredentials: true
         })
     }
