c14a7466dd4293a2569f47d1de4355b7088cadc2
davidvrl <veral.david@gmail.com>
2020-03-16 17:10:30 +0100

    Deploying

---
+++ server/public/precache-manifest.fa634c9484629a516adcb294894aa2c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b99a14548a78cbfa19efce43cc974c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88e653d40e74b24e3ab0",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d9b311ac8d7409a29e2e",
+    "url": "/static/css/main.7c6bf084.chunk.css"
+  },
+  {
+    "revision": "88e653d40e74b24e3ab0",
+    "url": "/static/js/2.c9c91e2a.chunk.js"
+  },
+  {
+    "revision": "ba1665840a4211d22917503a2a5544f6",
+    "url": "/static/js/2.c9c91e2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b311ac8d7409a29e2e",
+    "url": "/static/js/main.f1b4f670.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
